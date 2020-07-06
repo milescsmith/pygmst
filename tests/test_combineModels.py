d2322f39ad4eb2f4c2e9bddc62fa29453959d40a
Miles Smith <mileschristiansmith@gmail.com>
2020-07-06 22:32:43 +0000

    remove tmp directory usage, extra logging, swap check_output for run

--- tests/test_combineModels.py
+++ tests/test_combineModels.py
@@ -23,22 +23,22 @@ class TestCombineFunction(unittest.TestCase):
 
     def test_cluster(self):
 
-        with tempfile.TemporaryDirectory() as tmpdir:
-            final_model = combineModels(
-                mod=[self.model_1, self.model_2], cut_offs=[30, 70], tmpdir=tmpdir
-            )
+        # with tempfile.TemporaryDirectory() as tmpdir:
+        final_model = combineModels(
+            mod=[self.model_1, self.model_2], cut_offs=[30, 70], #tmpdir=tmpdir
+        )
 
-            with open(final_model, "r") as fm, open(
-                self.expected_combinedModels, "r"
-            ) as em:
-                final_model = "".join(fm.readlines())
-                expected_model = "".join(em.readlines())
+        with open(final_model, "r") as fm, open(
+            self.expected_combinedModels, "r"
+        ) as em:
+            final_model = "".join(fm.readlines())
+            expected_model = "".join(em.readlines())
 
-            self.assertMultiLineEqual(
-                final_model,
-                expected_model,
-                msg=f"function 'combineModels' failed: the resulting concatination did not match the expected one",
-            )
+        self.assertMultiLineEqual(
+            final_model,
+            expected_model,
+            msg=f"function 'combineModels' failed: the resulting concatination did not match the expected one",
+        )
 
 
 if __name__ == "__main__":
